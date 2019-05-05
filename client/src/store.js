ed790a674ba857caeb7566b2ee5bad2a5c0834df
Fabian Anguiano <fabiananguiano@gmail.com>
2019-05-05 14:33:30 -0500

    redux is breaking mobile

--- client/src/store.js
+++ client/src/store.js
@@ -9,10 +9,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
